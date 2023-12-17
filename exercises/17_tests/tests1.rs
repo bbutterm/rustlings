12873c7574b627a98fc45dc8eb7b1f6523afbc8f
bbutterm <ikomarov96@mail.ru>
2023-12-17 21:09:46 +0300

    day11

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -10,12 +10,13 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
+    
     fn you_can_assert() {
-        assert!();
+        let i_am_not_gay = true;
+        assert!(i_am_not_gay);
     }
 }
